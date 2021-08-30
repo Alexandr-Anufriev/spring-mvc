17c0307d4f501c62eb3ecac8ac4d7962c38ed45e
Alexandr-Anufriev <aav_93@mail.ru>
2021-08-30 19:30:32 +0300

    Version 1.0

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,21 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+public class CarService {
+
+    public static List<Car> carList(int limit) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Car1", "Model1", 1));
+        cars.add(new Car("Car2", "Model2", 2));
+        cars.add(new Car("Car3", "Model3", 3));
+        cars.add(new Car("Car4", "Model4", 4));
+        cars.add(new Car("Car5", "Model5", 5));
+        return cars.stream().limit(limit).collect(Collectors.toList());
+    }
+}
