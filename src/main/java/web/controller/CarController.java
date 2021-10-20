8e9f4feb813d9d7ad31001e8b8657f627f63ff46
Alexandr-Anufriev <aav_93@mail.ru>
2021-10-20 22:23:04 +0300

    Version 2.0 Добавил интерефейс Service, сделал метод в нестатическим в CarService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,18 +6,18 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.Service;
+import web.service.CarService;
 
 import java.util.List;
 
 @Controller()
 public class CarController {
     @Autowired
-    private Service service;
+    private CarService carService;
 
     @GetMapping("/cars")
     public String carList(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-        List<Car> cars = service.getCarList(count);
+        List<Car> cars = carService.getCarList(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,22 +1,9 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-@Service
-public class CarService implements web.service.Service {
 
-    @Override
-    public List<Car> getCarList(int limit) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Car1", "Model1", 1));
-        cars.add(new Car("Car2", "Model2", 2));
-        cars.add(new Car("Car3", "Model3", 3));
-        cars.add(new Car("Car4", "Model4", 4));
-        cars.add(new Car("Car5", "Model5", 5));
-        return cars.stream().limit(limit).collect(Collectors.toList());
-    }
+public interface CarService {
+    List<Car> getCarList(int limit);
 }
